a7f2a00ba243268eb4041065cecfcebfa32f8b22
nikita-reva <aniketos@gmx.de>
2022-09-20 10:27:56 +0200

    fix: fixed scss build script

--- packages/scss/src/scripts/build.js
+++ packages/scss/src/scripts/build.js
@@ -30,6 +30,10 @@ const compile = (path, fileName) => {
   Fs.writeFileSync(Path.resolve(fileName), result.css.toString());
 };
 
+try {
+  FS.mkdirSync(Path.resolve('lib'));
+} catch (e) {}
+
 compile('src/global.scss', 'lib/global.css');
 
 getComponents().forEach((component) => {
